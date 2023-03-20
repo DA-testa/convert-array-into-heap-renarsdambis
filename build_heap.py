56bc6e6061b96481d3c5329f0538d8cb6b875e99
renarsdambis <125255526+renarsdambis@users.noreply.github.com>
2023-03-20 01:40:54 +0000

    kk

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,37 @@
-# python3
-
-
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
+    # start from the middle of the array and iterate towards the beginning
+    for i in range(n // 2, -1, -1):
+        j = i
+        # keep swapping until the element is smaller than both its children or it becomes a leaf node
+        while j * 2 + 1 < n:
+            left = j * 2 + 1
+            right = j * 2 + 2 if j * 2 + 2 < n else left
+            if data[left] < data[right]:
+                smallest = left
+            else:
+                smallest = right
+            if data[j] > data[smallest]:
+                data[j], data[smallest] = data[smallest], data[j]
+                swaps.append((j, smallest))
+                j = smallest
+            else:
+                break
 
     return swaps
 
-
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
